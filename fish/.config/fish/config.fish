4cc1d4a2aa227b4f69e74bd74fd5bf3a70b400c2
drewcassidy <drewcassidy@me.com>
2017-12-09 20:58:26 -0800

    update itermcolors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,14 @@
+# setup location variables
+set FISH_PATH  "$HOME/.config/fish"
+set FLINE_PATH "$FISH_PATH/fishline"
 
 # setup FishLine
-set FLINE_PATH "$HOME/.config/fish/fishline"
 source $FLINE_PATH/init.fish
-source $FLINE_PATH/../fishline_colors.fish
-
+source $FISH_PATH/fishline_colors.fish
 set FLCLR_USER_BG           blue
 set FLCLR_USER_FG           white
+
+# setup colors
+set -gx GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+set -gx CLICOLOR 1
+set -gx LSCOLORS 'ExcxdxfxgxxexcxdxfBxBx'
