4ebc3fd1c9e96de84922ce6734ae5f147395ecc6
drewcassidy <drewcassidy@me.com>
2018-01-18 10:11:41 -0800

    Add things

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,12 @@
 set FISH_PATH  "$HOME/.config/fish"
 set FLINE_PATH "$FISH_PATH/fishline"
 
+set -gx PATH /usr/local/opt/curl/bin $PATH
+set -gx PATH /usr/local/opt/openssl/bin $PATH
+set -gx PATH ~/Library/Python/2.7/bin $PATH
+
+set -gx PKG_CONFIG_PATH /usr/local/opt/openssl/lib/pkgconfig $PKG_CONFIG_PATH
+
 # setup FishLine
 source $FLINE_PATH/init.fish
 source $FISH_PATH/fishline_colors.fish
