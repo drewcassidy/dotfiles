7a5fb0e621f407ae48eea52cba11720096b4dc87
drewcassidy <drewcassidy@me.com>
2018-02-10 00:21:05 -0800

    add fish colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,3 +19,25 @@ set -gx GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:qu
 set -gx CLICOLOR 1
 set -gx LSCOLORS 'ExcxdxfxgxxexcxdxfBxBx'
 
+set fish_color_autosuggestion 'brblack'
+set fish_color_cancel -r
+set fish_color_command --bold
+set fish_color_comment red
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end brmagenta
+set fish_color_error brred
+set fish_color_escape 'bryellow'  '--bold'
+set fish_color_history_current --bold
+set fish_color_host normal
+set fish_color_match --background=brblue
+set fish_color_normal normal
+set fish_color_operator bryellow
+set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection brblue
+set fish_color_search_match 'bryellow'  '--background=brblack'
+set fish_color_selection 'white'  '--bold'  '--background=brblack'
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+
